1dd65214f3afc2f67e1f26e870681f79363fd296
Gabriel <kenddygabriel841@gmail.com>
2025-04-05 16:07:44 -0300

    Update ResultClassif.js

--- src/components/ResultClassif.js
+++ src/components/ResultClassif.js
@@ -2,16 +2,16 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-
 const ResultClassif = ({ classificacao }) => {
     return (
-        <Text style={styles.Result}>Sua classificação de IMC é: {classificacao}</Text>
+        <Text style={styles.result}>Sua classificação de IMC é: {classificacao}</Text>
     );
 };
 
 const styles = StyleSheet.create({
     result: {
         marginTop: 20,
+        marginBottom: 20,
         fontSize: 24,
         textAlign: 'center',
         color: '#333',
