140a93417a74a462fb50129db9a9f1a3883b1add
Gabriel <kenddygabriel841@gmail.com>
2025-04-07 08:35:25 -0300

    Update ResultClassif.js

--- src/components/ResultClassif.js
+++ src/components/ResultClassif.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
+//arrow function/ tag que exibe a classificação do imc com base na classificação calculada anteriormente
 const ResultClassif = ({ classificacao }) => {
     return (
         <Text style={styles.result}>Sua classificação de IMC é: {classificacao}</Text>
     );
 };
 
+//css da tag
 const styles = StyleSheet.create({
     result: {
         marginTop: 20,
