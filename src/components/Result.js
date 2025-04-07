f13370319f4f38d2cc078225cb9c493aadc44412
Gabriel <kenddygabriel841@gmail.com>
2025-04-07 08:36:04 -0300

    Update Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
+//arrow function/ tag que exibe o imc calculado anteriormente na tela
 const Result = ({ imc }) => {
     return (
         <Text style={styles.result}>Seu IMC é: {imc}</Text>
     );
 };
 
+//css da tag
 const styles = StyleSheet.create({
     result: {
         marginTop: 20,
