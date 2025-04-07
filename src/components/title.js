4495541129618bd7f9af7f0b58e13ea994d21e45
Gabriel <kenddygabriel841@gmail.com>
2025-04-07 08:34:46 -0300

    Update title.js

--- src/components/title.js
+++ src/components/title.js
@@ -1,14 +1,14 @@
-//importa as tags que v�o ser utilizadas no c�digo (tag pra texto e pro css)
+//importa as tags que vão ser utilizadas no código (tag pra texto e pro css)
 import { Text, StyleSheet } from 'react-native';
 
-//arrow function que retorna uma tag ""html"" - styles.title s� serve para "dar um id" que ser� utilizado no css
+//arrow function que retorna uma tag ""html"" - styles.title só serve para "dar um id" que será utilizado no css
 const Title = () => {
     return (
         <Text style={styles.title}>Calculadora de IMC</Text>
     );
 };
 
-//css da tag text (com id title) armazenado em uma vari�vel 
+//css da tag text (com id title) armazenado em uma variável 
 const styles = StyleSheet.create({
     title: {
         fontSize: 32,
@@ -18,5 +18,5 @@ const styles = StyleSheet.create({
     },
 });
 
-//exporta a fun��o title
+//exporta a função title
 export default Title;
